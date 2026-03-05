02c8918f24cc130beeaac85ae5ce301aad9d3cf8
ShivSagar Dahayat <162616573+ShivsagarDahiya@users.noreply.github.com>
2026-03-05 18:25:02 +0530

    Update JwtProvider.java

--- src/main/java/com/satna/config/JwtProvider.java
+++ src/main/java/com/satna/config/JwtProvider.java
@@ -17,46 +17,40 @@ import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtProvider {
+	
+	private SecretKey key=Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+	
+	public String generateToken(Authentication auth) {
+		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+	    String roles = populateAuthorities(authorities);
 
-    private final SecretKey key =
-            Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+		String jwt=Jwts.builder()
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(new Date().getTime()+86400000))
+				.claim("email",auth.getName())
+				.claim("authorities", roles)
+				.signWith(key)
+				.compact();
+		return jwt;
+		
+	}
+	
+	public String getEmailFromJwtToken(String jwt) {
+		jwt=jwt.substring(7);
+		
+		Claims claims=Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+		String email=String.valueOf(claims.get("email"));
+		
+		return email;
+	}
+	
+	public String populateAuthorities(Collection<? extends GrantedAuthority> collection) {
+		Set<String> auths=new HashSet<>();
+		
+		for(GrantedAuthority authority:collection) {
+			auths.add(authority.getAuthority());
+		}
+		return String.join(",",auths);
+	}
 
-    public String generateToken(Authentication auth) {
-
-        String roles = populateAuthorities(auth.getAuthorities());
-
-        return Jwts.builder()
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .claim("email", auth.getName())
-                .claim("authorities", roles)
-                .signWith(key)
-                .compact();
-    }
-
-    public String getEmailFromJwtToken(String jwt) {
-
-        if (jwt.startsWith("Bearer ")) {
-            jwt = jwt.substring(7);
-        }
-
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(jwt)
-                .getBody();
-
-        return String.valueOf(claims.get("email"));
-    }
-
-    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
-
-        Set<String> auths = new HashSet<>();
-
-        for (GrantedAuthority authority : authorities) {
-            auths.add(authority.getAuthority());
-        }
-
-        return String.join(",", auths);
-    }
 }
