3ef0bb173c0d84272f50f1733eb5e5068af790db
ShivSagar Dahayat <162616573+ShivsagarDahiya@users.noreply.github.com>
2026-03-05 18:24:35 +0530

    Update JwtConstant.java

--- src/main/java/com/satna/config/JwtConstant.java
+++ src/main/java/com/satna/config/JwtConstant.java
@@ -2,8 +2,10 @@ package com.satna.config;
 
 public class JwtConstant {
 
-    public static final String SECRET_KEY =
-            "wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
 
-    public static final String JWT_HEADER = "Authorization";
+	public static final String SECRET_KEY="wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
+	public static final String JWT_HEADER="Authorization";
+	
 }
+
+//wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe old key
